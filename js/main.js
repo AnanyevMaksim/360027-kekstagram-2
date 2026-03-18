ee90c59b93e86579e6c75eca89830a6c8e6791e7
maxim.ananyev <maksim.ve@gmail.com>
2026-03-18 17:03:08 +0300

    Подключил новый модуль upload-photo-form.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import {renderThumbnails} from './render-thumbnails.js';
 import {generatePhotos} from './create-array-photos.js';
 import {openBigPicture} from './render-big-picture.js';
+import {initUploadForm} from './upload-photo-form.js';
 
 const userPhotos = generatePhotos();
 renderThumbnails(userPhotos, openBigPicture);
+initUploadForm();
